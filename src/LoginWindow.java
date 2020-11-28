5827e60aea899ea9f89f905c21d9afe5c3ca2c9e
Sameer-cell <70033858+Sameer-cell@users.noreply.github.com>
2020-11-28 15:44:49 +0530

    Update LoginWindow.java

--- src/LoginWindow.java
+++ src/LoginWindow.java
@@ -4,14 +4,13 @@ import java.awt.*;
 public class LoginWindow {
     JFrame window;
 
-    public void loginWindow(){
-        window=new JFrame();
-        window.setSize(800,750);
+    public void loginWindow() {
+        window = new JFrame();
+        window.setSize(800, 750);
         window.setLayout(null);
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        window.setLocation(400,20);
+        window.setLocation(400, 20);
         window.getContentPane().setBackground(new Color(51, 255, 247));
         window.setVisible(true);
     }
-
 }
